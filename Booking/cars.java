983484a444960f532baa5157a9f2b08c4ff9f0d0
Yori <yusufmj605@gmail.com>
2023-11-18 10:40:50 +0300

    edited the program to be highly cohesive and loosely coupled.

--- Booking/cars.java
+++ Booking/cars.java
@@ -1,22 +1,24 @@
-public class cars {
+public class Cars {
 
     private String brand;
     private String model;
     private int year;
     private int price;
 
-    public cars(){
+    public Cars(){
         this.brand = "brandless";
         this.model = "null";
         this.year =1970;
         this.price=0;
+        // default constructor, this should not be used except for testing.
     }
 
-    public cars(String brand, String model, int year, int price){
+    public Cars(String brand, String model, int year, int price){
         this.brand = brand;
         this.model = model;
         this.year = year;
         this.price= price;
+        //This constructor will generate an object from the Cars class, this will give us an easy access to add or examine a car.
     }
 
     public String getBrand(){
@@ -29,6 +31,7 @@ public class cars {
 
     public String toString() {
         return brand+" "+model+" "+year+" "+price;
+        //overwritting the default toString method as we want the details and the Cars object and not it's hashcode 
       }
     }
 
